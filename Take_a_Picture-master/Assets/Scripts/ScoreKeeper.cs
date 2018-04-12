8fbbd6d2ff5781b32913b5bfa5d66e655e143135
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-04-12 14:47:53 +0100

    Win screen fixed and treasure chest painting added back in.
    Woop

--- Take_a_Picture-master/Assets/Scripts/ScoreKeeper.cs
+++ Take_a_Picture-master/Assets/Scripts/ScoreKeeper.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ScoreKeeper : MonoBehaviour {
 
     public Text levelScore; //In the inspector drag the score number TEXT into this.
+    public Text totalScore;
     public static int score = 0;
     public int currentScore; // Stores the current score. Future proof in case there is a level 2.
     public int picturesInLevel = 0;
@@ -19,6 +20,7 @@ public class ScoreKeeper : MonoBehaviour {
     {
         score += points;//adds to what it currently equals or simply increased by.
         levelScore.text = "Pictures Taken: " + score.ToString() + (picturesInLevel==0f ? "" :"/"+picturesInLevel);
+        totalScore.text = score.ToString();
     }
 
     public void saveScore()
